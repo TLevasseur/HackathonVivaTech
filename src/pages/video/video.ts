4bfd56f2d5de6fcb5a15c7601e6a448322f015b4
Coralie Delpha <coralie.delpha@gmail.com>
2017-06-17 05:17:52 +0200

    ajout de nom et logo

--- src/pages/video/video.ts
+++ src/pages/video/video.ts
@@ -15,10 +15,14 @@ import {IonicPage, NavController, NavParams}from 'ionic-angular';
 export class VideoPage {
 
   url : Url;
+  name : string;
+  logoAddress : string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     var i = navParams.get("id");
     this.url = VIDEOS.filter( (e : Url) => e.id == i)[0];
+    this.name = navParams.get("name");
+    this.logoAddress = navParams.get("logo");
   }
 
   ionViewDidLoad() {
@@ -40,4 +44,5 @@ export const VIDEOS= [
     "url": "https://firebasestorage.googleapis.com/v0/b/nelson-88332.appspot.com/o/habilebill.mp4?alt=media&token=aa081f88-9a4c-4f9a-be40-da2fa1dc67bb"
   }
 ];
+
 
